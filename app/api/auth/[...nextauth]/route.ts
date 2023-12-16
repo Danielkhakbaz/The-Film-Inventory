2231ada3aa23328e3c098c79b293dbb47843d7fd
Danial <danial@Danials-MacBook-Pro.local>
2023-12-16 16:16:51 +0330

    feat: Add NextAuth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,19 @@
 import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import Prisma from "prisma/client/client";
 
 const handler = NextAuth({
+  adapter: PrismaAdapter(Prisma),
   providers: [
-    GoogleProvider({
+    Github({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  //   session: {
+  //     strategy: "jwt",
+  //   },
 });
 
 export { handler as GET, handler as POST };
