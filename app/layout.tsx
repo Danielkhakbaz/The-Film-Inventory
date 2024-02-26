db4acc3196e1223acbeae5580f1d55cea8f9a0ec
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-02-26 19:08:47 +0330

    (seo): Update the metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,9 +11,29 @@ type RootLayoutProps = {
 
 export const metadata: Metadata = {
   title: "The Film Inventory",
-  description: "Developed by Next14 as the full-stack framework.",
+  description:
+    "Welcome to The Film Inventory website where You can categorized all the animes in the world.",
   icons: {
-    icon: ["/images/favicon.png"],
+    icon: "/images/favicon.png",
+  },
+  other: {
+    "theme-color": "#000000",
+    "color-scheme": "dark",
+    "twitter:title": "The Film Inventory",
+    "twitter:description":
+      "Welcome to The Film Inventory website where You can categorized all the animes in the world.",
+    "twitter:url": "https://the-film-inventory.vercel.app/",
+    "twitter:domain": "the-film-inventory.vercel.app",
+    "twitter:image":
+      "https://ogcdn.net/6064b869-74ed-4eb9-b76c-0b701ffe7e6b/v4/the-film-inventory.vercel.app/The%20Film%20Inventory/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fdocuments%2F8a07c3e8-b81a-427b-b122-56169464ccee.jpg%3Ftoken%3DyM3DzPs8JZsawgrBzvRQrp2td8GPWMsJrcJFG5v4uJk%26height%3D795%26width%3D1200%26expires%3D33244961842/og.png",
+    "twitter:card": "summary_large_image",
+    "og:title": "The Film Inventory",
+    "og:description":
+      "Welcome to The Film Inventory website where You can categorized all the animes in the world.",
+    "og:url": "https://the-film-inventory.vercel.app/",
+    "og:image":
+      "https://ogcdn.net/6064b869-74ed-4eb9-b76c-0b701ffe7e6b/v4/the-film-inventory.vercel.app/The%20Film%20Inventory/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fdocuments%2F8a07c3e8-b81a-427b-b122-56169464ccee.jpg%3Ftoken%3DyM3DzPs8JZsawgrBzvRQrp2td8GPWMsJrcJFG5v4uJk%26height%3D795%26width%3D1200%26expires%3D33244961842/og.png",
+    "og:type": "website",
   },
 };
 
